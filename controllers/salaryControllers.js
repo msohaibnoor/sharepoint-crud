7db53c1cb5600d86cddaa2c54851649ff3bf62c3
unknown <sohaib.noor@aquila360.com>
2022-07-29 11:44:50 +0500

    completed crud of list

--- controllers/salaryControllers.js
+++ controllers/salaryControllers.js
@@ -18,7 +18,6 @@ let spr = sprequest.create(credentialOptions);
 
 const getSalaries = async (req, res) => {
   console.log("Reached here");
-  console.log(process.env.password, process.env.user);
   try {
     let response = await spr.get(
       "https://aquila360.sharepoint.com/sites/SPFxDevs/_api/lists/GetByTitle('Payslips')/Items?$select=Name,Month,Year,TotatSalary,Deduction,Reimbursements,ID"
@@ -45,4 +44,119 @@ const getSalaries = async (req, res) => {
   //   res.json({ products, page, pages: Math.ceil(count / pageSize) });
 };
 
-export { getSalaries };
+const addSalaryRecord = async (req, res) => {
+  console.log("Reached here");
+  spr
+    .requestDigest("https://aquila360.sharepoint.com/sites/SPFxDevs")
+    .then((digest) => {
+      return spr.post(
+        `https://aquila360.sharepoint.com/sites/SPFxDevs/_api/web/lists/GetByTitle('Payslips')/items`,
+        {
+          headers: {
+            "X-RequestDigest": digest,
+          },
+          body: {
+            __metadata: { type: `SP.Data.PayslipsListItem` },
+            Title: "Test",
+            Name: "Test",
+            Month: "Test",
+          },
+        }
+      );
+    })
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  // .then((response) => {
+  //   console.log(response.body.d.results);
+  // })
+  // .catch((err) => {
+  //   console.log(err);
+  // });
+
+  //   res.json({ products, page, pages: Math.ceil(count / pageSize) });
+};
+
+const updateSalaryRecord = async (req, res) => {
+  console.log("Reached here");
+  spr
+    .requestDigest("https://aquila360.sharepoint.com/sites/SPFxDevs")
+    .then((digest) => {
+      return spr.post(
+        `https://aquila360.sharepoint.com/sites/SPFxDevs/_api/web/lists/GetByTitle('Payslips')/items(4)`,
+        {
+          headers: {
+            "X-HTTP-Method": "MERGE",
+            "X-RequestDigest": digest,
+            "IF-MATCH": "*",
+          },
+          body: {
+            __metadata: { type: `SP.Data.PayslipsListItem` },
+            Title: "Test",
+            Name: "Test",
+            Month: "Test",
+          },
+        }
+      );
+    })
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  // .then((response) => {
+  //   console.log(response.body.d.results);
+  // })
+  // .catch((err) => {
+  //   console.log(err);
+  // });
+
+  //   res.json({ products, page, pages: Math.ceil(count / pageSize) });
+};
+
+const deleteSalaryRecord = async (req, res) => {
+  console.log("Reached here");
+  spr
+    .requestDigest("https://aquila360.sharepoint.com/sites/SPFxDevs")
+    .then((digest) => {
+      return spr.delete(
+        `https://aquila360.sharepoint.com/sites/SPFxDevs/_api/web/lists/GetByTitle('Payslips')/items(5)`,
+        {
+          headers: {
+            "X-HTTP-Method": "DELETE",
+            "X-RequestDigest": digest,
+            "IF-MATCH": "*",
+          },
+          // body: {
+          //   __metadata: { type: `SP.Data.PayslipsListItem` },
+          //   Title: "Test",
+          //   Name: "Test",
+          //   Month: "Test",
+          // },
+        }
+      );
+    })
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  // .then((response) => {
+  //   console.log(response.body.d.results);
+  // })
+  // .catch((err) => {
+  //   console.log(err);
+  // });
+
+  //   res.json({ products, page, pages: Math.ceil(count / pageSize) });
+};
+
+export { getSalaries, addSalaryRecord, deleteSalaryRecord, updateSalaryRecord };
--- routes/salaryRoutes.js
+++ routes/salaryRoutes.js
@@ -1,7 +1,17 @@
 import express from "express";
 const router = express.Router();
-import { getSalaries } from "../controllers/salaryControllers.js";
+import {
+  getSalaries,
+  addSalaryRecord,
+  deleteSalaryRecord,
+  updateSalaryRecord,
+} from "../controllers/salaryControllers.js";
 
-router.route("/").get(getSalaries);
+router
+  .route("/")
+  .get(getSalaries)
+  .post(addSalaryRecord)
+  .delete(deleteSalaryRecord)
+  .put(updateSalaryRecord);
 
 export default router;
